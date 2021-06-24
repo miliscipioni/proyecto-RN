e0a3e3efa08f135ee9fd6740f7d6004889914125
miliscipioni <scipionimilagros@gmail.com>
2021-06-24 01:16:04 -0300

    arreglos en RandomUser.js

--- src/api/RandomUsers.js
+++ src/api/RandomUsers.js
@@ -1,11 +1,7 @@
 
 export async function getData(n) {
     try {
-<<<<<<< Updated upstream
-        let resultado = await fetch('https://randomuser.me/api?results='+n);
-=======
-        let resultado = await fetch('https://randomuser.me/api?results='+ n );
->>>>>>> Stashed changes
+        let resultado = await fetch('https://randomuser.me/api?results='+n)
         let json = await resultado.json();
         return json.results;
     } catch(error) {
